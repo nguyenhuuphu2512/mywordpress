830c0ef73b631b7b3cfe6ce89e7c216b0f6599ef
nguyenhuuphu2512 <huuphunguyen62@gmail.com>
2021-10-18 07:15:52 +0700

    dsf

--- wp-login.php
+++ wp-login.php
@@ -2,7 +2,7 @@
 <html lang="vi">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Đăng nhập &lsaquo; WordPress &#8212; WordPress</title>
+	<title>Đăng nhập &lsaquo; Shop giày Phú Nguyễn &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="https://nguyenhuuphu2512.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
@@ -55,7 +55,7 @@
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://nguyenhuuphu2512.github.io/mywordpress/">&larr; Quay lại WordPress</a>		</p>
+			<a href="https://nguyenhuuphu2512.github.io/mywordpress/">&larr; Quay lại Shop giày Phú Nguyễn</a>		</p>
 			</div>
 	<script type="text/javascript" src="https://nguyenhuuphu2512.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
 <script type="text/javascript" src="https://nguyenhuuphu2512.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
@@ -94,7 +94,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 <script type="text/javascript" src="https://nguyenhuuphu2512.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"c35ec1c1ca"};
+var userProfileL10n = {"user_id":"0","nonce":"2cd55889cb"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
